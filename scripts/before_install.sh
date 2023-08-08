91b5f2b0f368a947c91e078cff780a8363ab5724
NessTest-dev <141342048+NessTest-dev@users.noreply.github.com>
2023-08-08 17:10:01 +0300

    Add files via upload

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# navigate to app folder
-cd /app
+# # navigate to app folder
+# cd /app
 
-# install node and npm
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-yum -y install nodejs npm
+# # install node and npm
+# curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+# yum -y install nodejs npm
+
+cd /home/ec2-user/S3
+aws s3 cp s3://codepipeline-us-east-1-213465850471/React-pipeline/BuildArtif ./ --recursive
